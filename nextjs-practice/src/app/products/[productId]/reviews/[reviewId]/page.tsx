4a2ed8601f47affdca9505100a3ad196f5d29e35
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-06-15 23:50:31 +0530

    added error boundary at reviewId route segment to catch error

--- nextjs-practice/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ nextjs-practice/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
+function getRandomInt(count: number){
+  return 2;
+  // return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewDetails({params}:{params:Promise<{productId:string, reviewId:string}>}) {
+
+  const randomNumber = getRandomInt(2);
+  console.log("randomNumber", randomNumber);
+  if(randomNumber == 2){
+    throw new Error("loading product review error");
+  }
+
   const {productId, reviewId} = await params;
   return (
     <div>
