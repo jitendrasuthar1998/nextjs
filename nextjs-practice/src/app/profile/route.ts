e9b84c8fa5f5e35f594620aaef00d6a6683b0f82
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-06-22 11:30:00 +0530

    set content-type header in response

--- nextjs-practice/src/app/profile/route.ts
+++ nextjs-practice/src/app/profile/route.ts
@@ -1,11 +1,21 @@
 import type {NextRequest} from "next/server";
+import {headers} from "next/headers";
 
 // reading authorization header value from request
 
 // if authorization header has a valid token, that time user can access resources.
 
 export async function GET(request:NextRequest) {
-    const requestHeaders = new Headers(request.headers);
-    console.log("requestHeaders", requestHeaders.get("accept"));
-    return new Response("Reached profile path");
+    // const requestHeaders = new Headers(request.headers);
+    // console.log("requestHeaders", requestHeaders.get("accept"));
+
+    // const headersList = await headers();
+    // console.log("headersList", headersList);
+    
+    return new Response("<h1>Reached profile path opened in chrome</h1>", {
+        headers:{
+            "Content-Type":"text/html"
+        }
+    });
 }
+
