7b40cc04d2ab1518835015458a4b3bf6cf6e6ee8
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-06-23 22:30:00 +0530

    working on setting up auth flow using cookies, authorizatoin header etc

--- nextjs-practice/src/app/(auth)/login/page.tsx
+++ nextjs-practice/src/app/(auth)/login/page.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 import React from 'react'
 
-const Login = async() => {
+const Login = () => {
 
-  await new Promise((resolve) => {
-    setTimeout(()=> {
-      resolve("Intentional Delay");
-    },2000)
-  })
+  // await new Promise((resolve) => {
+  //   setTimeout(()=> {
+  //     resolve("Intentional Delay");
+  //   },2000)
+  // })
+
+  // return (
+  //   <div>Login</div>
+  // )
+
+  // login with username and password
+
+  async function handleLogin(e:React.FormEvent) {
+    e.preventDefault();
+
+    const res = await fetch("/api/login", {
+      method:"POST",
+      body: JSON.stringify({username:"admin", password:"123456"})
+    })
+
+    if(res.ok){
+      alert("Logged in successfully.");
+    }
+    else{
+      alert("Login failed");
+    }
+  }
 
   return (
-    <div>Login</div>
+    <form onSubmit={handleLogin}>
+      <button type="submit">Login as Admin</button>
+    </form>
   )
 }
 
