c031dd034fcebf9f1010ea7c5f8cbc5013bff27c
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-06-29 10:30:00 +0530

    implemented caching with revalidate data after 10 seconds

--- nextjs-practice/src/app/api/time/route.ts
+++ nextjs-practice/src/app/api/time/route.ts
@@ -1,5 +1,9 @@
 export const dynamic = "force-static"
 
+// data will be cached for every 10 seconds.
+
+export const revalidate = 10
+
 export async function GET() {
     return Response.json({time: new Date().toLocaleTimeString()});
 }
