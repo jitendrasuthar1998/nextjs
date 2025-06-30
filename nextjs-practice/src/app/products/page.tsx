5101dc74383cc75ed9f1d5a20fcf72be26f78d8e
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-06-30 22:30:00 +0530

    fetched products from dummyjson products api

--- nextjs-practice/src/app/products/page.tsx
+++ nextjs-practice/src/app/products/page.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 
-export default function Products() {
+export default async function Products() {
+  // fetching data from api call like from database, and taking lot of times.
+
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  const products = data.products;
+
   return (
     <div>
-        <h1>Product list</h1>
-        <ul>
-          <li><Link href={"/products/1"}>Product 1</Link></li>
-          <li><Link href={"/products/2"}>Product 2</Link></li>
-          <li><Link href={"/products/3"}>Product 3</Link></li>
-          <li><Link href={"/products/4"}>Product 4</Link></li>
-        </ul>
-        
-        
-        
-        
+      <h1>Product list</h1>
+      <ul>
+        {products.map((product: { id: string; title: string }) => (
+          <li key={product?.id}>
+            <Link href={`products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+
+        {/* <li>
+          <Link href={"/products/2"}>Product 2</Link>
+        </li>
+        <li>
+          <Link href={"/products/3"}>Product 3</Link>
+        </li>
+        <li>
+          <Link href={"/products/4"}>Product 4</Link>
+        </li> */}
+      </ul>
     </div>
-  )
+  );
 }
