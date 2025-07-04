28487e2142cf3566804052daafe5efb217cb82b0
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-07-04 22:30:00 +0530

    Understanding how client side rendering works in React Server Component architecture

--- nextjs-practice/src/app/blog/first/page.tsx
+++ nextjs-practice/src/app/blog/first/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React from 'react'
 
 export default function FirstBlog() {
+  console.log("FirstBlog rendered on both server and client side");
   return (
     <div>FirstBlog</div>
   )
