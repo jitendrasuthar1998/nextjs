bf0443d1a38b5fe182336766238b6732948971f2
jitendrasuthar1998 <jitendrasuthar995@gmail.com>
2025-07-05 22:30:00 +0530

    Created a client side component and used useState hook to control the input field state

--- nextjs-practice/src/app/about/page.tsx
+++ nextjs-practice/src/app/about/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+"use client";
+import React, {useState} from 'react'
 
 const About = () => {
+  const [name, setName] = useState("");
   return (
-    <div>About</div>
+    <div>
+      <h1>About</h1>
+      <input name="about" placeholder="about" value={name} onChange={(e)=> setName(e.target.value)}/>
+
+      <p>Hello {name}</p>
+    </div>
   )
 }
 
